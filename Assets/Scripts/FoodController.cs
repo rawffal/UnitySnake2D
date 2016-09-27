e6a4baec3a765ea6c30dc9fa7b47625d1017bf55
rawffal <ccccplz@hotmail.com>
2016-09-27 11:37:28 -0700

    Add the Snake player and food. Need to add the "Game Over" screen.

---
+++ Assets/Scripts/FoodController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodController : MonoBehaviour {
+
+    public GameObject food;
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", 3, 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn()
+    {
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        int y = (int)Random.Range(borderTop.position.y, borderBottom.position.y);
+        Vector2 position = new Vector2(x, y);
+        Instantiate(food, position, Quaternion.identity);   
+    }
+}
---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SnakeController : MonoBehaviour {
+
+    Vector2 dir = Vector2.right;
+
+    List<Transform> tail = new List<Transform>();
+
+    bool ate = false;
+
+    public GameObject tailPrefab;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Move", 0.3f, 0.3f); //300 milliseconds
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir = Vector2.right;
+        } 
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir = -Vector2.right;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            dir = Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            dir = -Vector2.up;
+        }
+	}
+
+    void Move()
+    {
+        // Save the position with the gap
+        Vector2 currentPosition = transform.position;
+
+        // Move the head
+        transform.Translate(dir);
+
+        if (ate)
+        {
+            GameObject tailObject = (GameObject)Instantiate(tailPrefab, currentPosition, Quaternion.identity);
+            tail.Insert(0, tailObject.transform);
+            ate = false;
+        }
+        // Check if the snake has a tail
+        else if (tail.Count > 0)
+        {
+            tail.Last().position = currentPosition;
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name.StartsWith("Food"))
+        {
+            ate = true;
+            Destroy(other.gameObject);
+        }
+        else // collided with something else besides the food and you lose
+        {
+            
+        }
+    }
+}
